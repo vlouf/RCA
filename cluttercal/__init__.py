5d46a6c63571611cfd7c6ef395cac5dbde967255
Valentin Louf <valentin.louf@bom.gov.au>
2020-03-30 11:12:28 +1100

    RENAME files/dirs

---
+++ cluttercal/__init__.py
@@ -0,0 +1,2 @@
+from cluttercal import extract_clutter, composite_mask, single_mask
+from cluttermask import clutter_mask
--- rca/__init__.py
+++
@@ -1,2 +0,0 @@
-from rca import extract_clutter, composite_mask, single_mask
-from cluttermask import clutter_mask
